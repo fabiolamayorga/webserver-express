1f395228c6af1302bc869deb931cfb46e31808e6
Fabiola Mayorga <fab.tames@gmail.comgit config --global user.name Fabiola Mayorga>
2019-03-12 12:15:07 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express();
+
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+const port  = process.env.PORT || 3000;
+
+
+app.use( express.static( __dirname + '/public' ) );
+
+//Express hbs engine
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+
+ 
+app.get('/', function (req, res) {
+  res.render('home', {
+      nombre: 'Fabiola',
+      anio: new Date().getFullYear()
+  });
+});
+
+app.get('/about', function (req, res) {
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
+  });
+ 
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`)
+});
